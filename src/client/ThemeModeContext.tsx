94b4fb012211c5e144a99e711c1b83f8521c7a45
Paweł Chwalczyk <pawel.chwalczyk@gmail.com>
2025-06-21 01:02:19 +0200

    TRIVIAL: Layout

---
+++ src/client/ThemeModeContext.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { createTheme, type Theme } from "@mui/material";
+import { lightThemeOptions } from "./theme.light";
+import { darkThemeOptions } from "./theme.dark";
+import { useMemo } from "react";
+
+type ThemeModeType = "light" | "dark";
+
+type ThemeModeContextType = {
+  theme: Theme;
+  themeMode: ThemeModeType;
+  setThemeMode: React.Dispatch<React.SetStateAction<ThemeModeType>>;
+};
+
+type ThemeModeProviderProps = {
+  children: React.ReactNode;
+};
+
+export const ThemeModeContext =
+  React.createContext<ThemeModeContextType | null>(null);
+
+export const useThemeMode = () => {
+  const context = React.useContext(ThemeModeContext);
+  if (!context) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+  return context;
+};
+
+const ThemeModeProvider = ({ children }: ThemeModeProviderProps) => {
+  const lightTheme = useMemo(() => createTheme(lightThemeOptions), []);
+  const darkTheme = useMemo(() => createTheme(darkThemeOptions), []);
+
+  const [themeMode, setThemeMode] = React.useState<ThemeModeType>("light");
+  const theme = useMemo(
+    () => (themeMode === "light" ? lightTheme : darkTheme),
+    [themeMode],
+  );
+
+  return (
+    <ThemeModeContext.Provider value={{ theme, themeMode, setThemeMode }}>
+      {children}
+    </ThemeModeContext.Provider>
+  );
+};
+
+export default ThemeModeProvider;
