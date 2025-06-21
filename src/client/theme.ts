1f9e69767e0020cf778e079b0a3c463d4c6cc189
Paweł Chwalczyk <pawel.chwalczyk@gmail.com>
2025-06-21 18:13:08 +0200

    TRIVIAL: Improve layout handling

---
+++ src/client/theme.ts
@@ -0,0 +1,81 @@
+import {createTheme} from "@mui/material";
+
+declare module "@mui/material/styles" {
+  interface TypeText {
+    hint: string | undefined;
+  }
+}
+
+const theme = createTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        mode: "light",
+        primary: {
+          main: "#d19af9",
+          contrastText: "#000000",
+        },
+        secondary: {
+          main: "#b0b0b0",
+          contrastText: "#000000",
+        },
+        divider: "#10becb",
+        text: {
+          primary: "rgb(0, 0, 0)",
+          secondary: "rgba(0, 0, 0, 0.6)",
+          disabled: "rgba(0, 0, 0, 0.38)",
+          hint: "rgb(16, 190, 203)",
+        },
+        background: {
+          default: "#ebebeb",
+        },
+      },
+    },
+    dark: {
+      palette: {
+        mode: "dark",
+        primary: {
+          main: "#3d0665",
+          contrastText: "#ffffff",
+        },
+        secondary: {
+          main: "#4f4f4f",
+          contrastText: "#ffffff",
+        },
+        divider: "#34e2ef",
+        text: {
+          primary: "rgb(255, 255, 255)",
+          secondary: "rgba(255, 255, 255, 0.6)",
+          disabled: "rgba(255, 255, 255, 0.38)",
+          hint: "rgb(52, 226, 239)",
+        },
+        background: {
+          default: "#141414",
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: "IBM Plex Sans",
+    fontWeightBold: 700,
+    fontWeightMedium: 500,
+    fontWeightRegular: 400,
+    fontWeightLight: 300,
+  },
+  cssVariables: {
+    colorSchemeSelector: "class",
+  },
+  defaultColorScheme: "dark",
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          padding: 0,
+          margin: 0,
+        },
+      },
+    },
+  },
+});
+
+export default theme;
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dotenv from "dotenv";
+import svgr from "vite-plugin-svgr";
 
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   build: { outDir: "dist/client" },
   base: process.env.BASE ?? "/web",
 });
